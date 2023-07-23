c5851205a6a7f2eb37563ad92d2b81c86b600543
Cierra_Runis <2864283875@qq.com>
2023-07-24 01:18:40 +0800

    📖删去 `riverpod`

--- lib/index.dart
+++ lib/index.dart
@@ -19,7 +19,6 @@ export 'package:cupertino_icons/cupertino_icons.dart';
 export 'package:dart_date/dart_date.dart' hide Interval;
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:flutter_localizations/flutter_localizations.dart';
-export 'package:flutter_riverpod/flutter_riverpod.dart';
 export 'package:loading_animation_widget/loading_animation_widget.dart'; // 加载器组件
 export 'package:intl/intl.dart' hide TextDirection;
 export 'package:isar/isar.dart';
