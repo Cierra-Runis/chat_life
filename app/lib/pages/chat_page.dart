93be1357ee7ae53df9c12aeaec84b3c6df4c4c5f
Cierra_Runis <2864283875@qq.com>
2023-11-12 23:46:31 +0800

    🌍Add file attachment, new PhotoView, link preview

--- app/lib/pages/chat_page.dart
+++ app/lib/pages/chat_page.dart
@@ -83,8 +83,11 @@ class _ChatPageState extends State<ChatPage> {
         ),
         theme: theme,
         messages: _messages,
+        disableImageGallery: true,
         onSendPressed: _handleSendPressed,
-        onAttachmentPressed: _handleImageSelection,
+        onMessageTap: _handleMessageTap,
+        onAttachmentPressed: _handleAttachmentPressed,
+        onPreviewDataFetched: _handlePreviewDataFetched,
         user: _user,
       ),
     );
@@ -92,6 +95,28 @@ class _ChatPageState extends State<ChatPage> {
 
   void _addMessage(types.Message message) {
     setState(() => _messages.insert(0, message));
+  }
+
+  void _handleMessageTap(BuildContext _, types.Message message) async {
+    if (message is types.FileMessage) {
+      await OpenFilex.open(message.uri);
+    }
+    if (message is types.ImageMessage) {
+      if (mounted) {
+        await context.push(
+          Scaffold(
+            appBar: AppBar(
+              title: const Text('图片预览'),
+            ),
+            body: Center(
+              child: Placeholder(
+                child: Text(message.uri),
+              ),
+            ),
+          ),
+        );
+      }
+    }
   }
 
   void _handleSendPressed(types.PartialText message) {
@@ -103,6 +128,68 @@ class _ChatPageState extends State<ChatPage> {
     );
 
     _addMessage(textMessage);
+  }
+
+  void _handleAttachmentPressed() {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) => SafeArea(
+        child: SizedBox(
+          height: 144,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _handleImageSelection();
+                },
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('Photo'),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  _handleFileSelection();
+                },
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('File'),
+                ),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text('Cancel'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleFileSelection() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.any,
+    );
+
+    if (result != null && result.files.single.path != null) {
+      final message = types.FileMessage(
+        author: _user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        id: randomString(),
+        name: result.files.single.name,
+        size: result.files.single.size,
+        uri: result.files.single.path!,
+      );
+
+      _addMessage(message);
+    }
   }
 
   void _handleImageSelection() async {
@@ -129,5 +216,19 @@ class _ChatPageState extends State<ChatPage> {
 
       _addMessage(message);
     }
+  }
+
+  void _handlePreviewDataFetched(
+    types.TextMessage message,
+    types.PreviewData previewData,
+  ) {
+    final index = _messages.indexWhere((element) => element.id == message.id);
+    final updatedMessage = (_messages[index] as types.TextMessage).copyWith(
+      previewData: previewData,
+    );
+
+    setState(() {
+      _messages[index] = updatedMessage;
+    });
   }
 }
